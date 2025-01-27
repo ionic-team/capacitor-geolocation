e384d92f83a4fff629e4d35dd272b79d9cc7bb5c
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-01-27 08:08:24 +0000

    chore: Fix iOS CI with Package.swift

--- packages/capacitor-plugin/Package.swift
+++ packages/capacitor-plugin/Package.swift
@@ -13,11 +13,17 @@ let package = Package(
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main")
     ],
     targets: [
+        .binaryTarget(
+            name: "IONGeolocationLib",
+            url: "https://github.com/ionic-team/ion-ios-geolocation/releases/download/1.0.0/IONGeolocationLib.zip",
+            checksum: "b117d3681a947f5d367e79abdb3bfc9abf7ab070ea5279d7da634ddd2d54ffdb" // sha-256
+        ),
         .target(
             name: "GeolocationPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                "IONGeolocationLib"
             ],
             path: "ios/Sources/GeolocationPlugin"),
         .testTarget(
