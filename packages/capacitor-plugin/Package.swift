6624ff2b8e99479e050a2da44613da487b071799
Rui Mendes <rui.moreira.mendes@outsystems.com>
2025-08-11 17:42:39 +0100

    update pod version

--- packages/capacitor-plugin/Package.swift
+++ packages/capacitor-plugin/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IONGeolocationLib",
-            url: "https://github.com/ionic-team/ion-ios-geolocation/releases/download/1.0.1/IONGeolocationLib.zip",
+            url: "https://github.com/ionic-team/ion-ios-geolocation/releases/download/1.0.0/IONGeolocationLib.zip",
             checksum: "b117d3681a947f5d367e79abdb3bfc9abf7ab070ea5279d7da634ddd2d54ffdb" // sha-256
         ),
         .target(
