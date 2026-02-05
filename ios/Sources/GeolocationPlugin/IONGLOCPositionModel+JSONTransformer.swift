e8031a828f8abb966928868326cf354ace80586b
Chace Daniels <chace.daniels@outsystems.com>
2026-02-05 09:45:40 -0600

    update json transformer

--- ios/Sources/GeolocationPlugin/IONGLOCPositionModel+JSONTransformer.swift
+++ ios/Sources/GeolocationPlugin/IONGLOCPositionModel+JSONTransformer.swift
@@ -17,7 +17,7 @@ extension IONGLOCPositionModel {
             Constants.Position.magneticHeading: magneticHeading != -1.0 ? magneticHeading : NSNull(),
             Constants.Position.trueHeading: trueHeading != -1.0 ? trueHeading : NSNull(),
             Constants.Position.headingAccuracy: headingAccuracy != -1.0 ? headingAccuracy : NSNull(),
-            Constants.Position.course: course,
+            Constants.Position.course: course != -1.0 ? course : NSNull(),
             Constants.Position.accuracy: horizontalAccuracy,
             Constants.Position.latitude: latitude,
             Constants.Position.longitude: longitude,
