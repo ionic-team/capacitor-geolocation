29af523311a91bc6766dc1f782ba367c0deff1fc
jcesarmobile <jcesarmobile@gmail.com>
2026-07-01 14:15:35 +0200

    chore!: Align plugin baselines with core

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorGeolocation",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "CapacitorGeolocation",
             targets: ["GeolocationPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5"),
         .package(url: "https://github.com/ionic-team/ion-ios-geolocation.git", from: "2.1.0")
     ],
     targets: [
