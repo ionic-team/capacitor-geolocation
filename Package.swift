b8e6127605af1e99794b4ae8ff8192d81802ae50
jcesarmobile <jcesarmobile@gmail.com>
2026-09-22 18:19:01 +0200

    chore: point SPM to Capacitor source repository (#102)

--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
             targets: ["GeolocationPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5"),
+        .package(url: "https://github.com/ionic-team/capacitor.git", from: "9.0.0-alpha.7"),
         .package(url: "https://github.com/ionic-team/ion-ios-geolocation.git", from: "2.1.0")
     ],
     targets: [
         .target(
             name: "GeolocationPlugin",
             dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Capacitor", package: "capacitor"),
                 .product(name: "IONGeolocationLib", package: "ion-ios-geolocation")
             ],
             path: "ios/Sources/GeolocationPlugin"),
